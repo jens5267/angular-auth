01439954cf7f0592d2696304c8d67732fd4b1476
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-22 22:28:39 +0500

    :heavy_check_mark: used validation system for register page

--- angular-auth/src/app/pages/register/register.component.ts
+++ angular-auth/src/app/pages/register/register.component.ts
@@ -1,6 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +14,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  form!: FormGroup;
+  registerForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -18,14 +23,30 @@ export class RegisterComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      name: '',
-      email: '',
-      password: '',
+    this.registerForm = this.formBuilder.group({
+      // Form validation
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
-
+  // get email data from input
+  get email() {
+    return this.registerForm.get('email');
+  }
+  // get username data from input
+  get name() {
+    return this.registerForm.get('name');
+  }
+  // get password data from input
+  get password() {
+    return this.registerForm.get('password');
+  }
+  // submit function
+  // check form valid & send POST request to backend
   submit(): void {
-    console.log(this.form.getRawValue());
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.getRawValue());
+    }
   }
 }
