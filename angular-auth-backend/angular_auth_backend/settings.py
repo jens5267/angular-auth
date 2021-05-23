449892964f4e7920e4f016eab44e98a21b2aa464
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-23 20:41:03 +0500

    :rocket: defined rest framework auth classes

--- angular-auth-backend/angular_auth_backend/settings.py
+++ angular-auth-backend/angular_auth_backend/settings.py
@@ -46,11 +46,14 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.AllowAny"
     ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser"
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ]
 }
 
 MIDDLEWARE = [
