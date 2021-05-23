4aaa4186ac8ddca85d1ea3c2d58fa4f55ee44ca2
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-23 20:40:24 +0500

    :m:odels moved to app dir

--- angular-auth-backend/angular_auth_backend/views.py
+++ angular-auth-backend/angular_auth_backend/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 
-from .models import Person
+from authentication.models import Person
 from .serizalizers import PersonSerizalizer
 
 
--- angular-auth-backend/authentication/admin.py
+++ angular-auth-backend/authentication/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from angular_auth_backend.models import Person
+from .models import Person
 
 
 admin.site.register(Person)
