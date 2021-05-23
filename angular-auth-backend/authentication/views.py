e2c242c4c4f84d815d94676a536fe006062aba4e
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-23 20:19:21 +0500

    ✨add `/register` endpoint's api view

--- angular-auth-backend/authentication/views.py
+++ angular-auth-backend/authentication/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import generics
+
+from .serializer import UserSerializer
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
