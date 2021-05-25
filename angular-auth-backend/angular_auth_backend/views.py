ea623d2bb09476760732ef6d629017f5fe1135ea
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-26 00:33:53 +0500

    ✨ simple route

--- angular-auth-backend/angular_auth_backend/views.py
+++ angular-auth-backend/angular_auth_backend/views.py
@@ -1,17 +1,5 @@
 from django.http import JsonResponse
 
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
-
-from authentication.models import Person
-from .serizalizers import PersonSerizalizer
-
-
-class ListPersons(generics.ListAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerizalizer
-    permission_classes = (IsAuthenticated,)
 
 def HomePage(request):
-    return JsonResponse({'message':'Hi bro!'})
+    return JsonResponse({'message': 'Hi bro 😎'})
