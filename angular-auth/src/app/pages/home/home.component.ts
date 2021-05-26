1fc45223ae03e2b89d712c7e8216e5ac9ec5e765
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-26 22:32:33 +0500

    :clock1: added token refresh functionality

--- angular-auth/src/app/pages/home/home.component.ts
+++ angular-auth/src/app/pages/home/home.component.ts
@@ -1,15 +1,32 @@
+import { UserService } from './../../services/user.service';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private cookie: CookieService,
+    private http: HttpClient,
+    private user: UserService
+  ) {}
 
   ngOnInit(): void {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.cookie.get('jwt')}`,
+    });
+    this.http
+      .get(`${environment.backend}/api/auth/users/me/`, { headers: headers })
+      .subscribe(
+        (res: any) => {},
+        (err: any) => {
+          this.user.refresh();
+        }
+      );
   }
-
 }
