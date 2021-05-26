38a75fee459b0d29c62ac64ccac22a1d8eb329ea
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-26 21:37:51 +0500

    🎈Api call , http request , error handling and etc.

--- angular-auth/src/app/pages/register/register.component.ts
+++ angular-auth/src/app/pages/register/register.component.ts
@@ -1,12 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
-  FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-register',
@@ -14,18 +14,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  type: any;
+  message: any;
   registerForm!: FormGroup;
-
   constructor(
     private formBuilder: FormBuilder,
-    private http: HttpClientModule,
+    private http: HttpClient,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       // Form validation
-      name: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
     });
@@ -35,18 +36,32 @@ export class RegisterComponent implements OnInit {
     return this.registerForm.get('email');
   }
   // get username data from input
-  get name() {
-    return this.registerForm.get('name');
+  get username() {
+    return this.registerForm.get('username');
   }
   // get password data from input
   get password() {
     return this.registerForm.get('password');
   }
   // submit function
-  // check form valid & send POST request to backend
+  // form validation then send POST request to backend
   submit(): void {
     if (this.registerForm.valid) {
-      console.log(this.registerForm.getRawValue());
+      let input_data = this.registerForm.getRawValue();
+      this.http
+        .post(`${environment.backend}/api/auth/users/`, input_data)
+        .subscribe(
+          (res: any) => {
+            // register success , navigate to login page
+            this.router.navigate(['/login']);
+          },
+          (err: any) => {
+            if (err['status'] === 400) {
+              this.type = 'alert-danger';
+              this.message = 'This username already taken';
+            }
+          }
+        );
     }
   }
 }
