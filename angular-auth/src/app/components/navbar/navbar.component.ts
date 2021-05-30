0541f14d7da91f7812ea8d4360577cbaf8db073c
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-31 00:42:24 +0500

    🔑 defined logout func & is authenticated

--- angular-auth/src/app/components/navbar/navbar.component.ts
+++ angular-auth/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
+import { UserService } from './../../services/user.service';
+import { Emitters } from './../../emitters/emitters';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  authenticated: boolean = false;
+  constructor(private user: UserService) {}
 
   ngOnInit(): void {
+    Emitters.authEmitter.subscribe((auth: boolean) => {
+      this.authenticated = auth;
+    });
   }
 
+  submit(): void {
+    this.user.logout()
+  }
 }
