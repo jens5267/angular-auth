2f8dd4a10b0578d7537206c040600e3976400ebb
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-05-31 00:43:46 +0500

    ♻for verifying used user service

--- angular-auth/src/app/pages/home/home.component.ts
+++ angular-auth/src/app/pages/home/home.component.ts
@@ -1,38 +1,19 @@
+import { Emitters } from './../../emitters/emitters';
 import { CookieService } from 'ngx-cookie-service';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { environment } from './../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(
-    private cookie: CookieService,
-    private http: HttpClient,
-  ) {}
+  message: any;
+  jwt: string = this.cookie.get('jwt');
+  refresh_token: string = this.cookie.get('refresh');
+  constructor(private cookie: CookieService, private user: UserService) {}
 
   ngOnInit(): void {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${this.cookie.get('jwt')}`,
-    });
-    this.http
-      .get(`${environment.backend}/api/auth/users/me/`, { headers: headers })
-      .subscribe(
-        (res: any) => {},
-        (err: any) => {
-          this.refresh();
-        }
-      );
-  }
-  refresh(this: any) {
-    let data = { refresh: this.cookie.get('refresh') };
-    this.http
-      .post(`${environment.backend}/api/auth/jwt/refresh/`, data)
-      .subscribe((res: any) => {
-        this.cookie.set('jwt', res['access']);
-      });
+      this.user.verify(this.jwt, this.refresh_token);
   }
 }
