cd4c76d8af2aee9385f806bbb43239ec99bc4aeb
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-06-03 18:16:26 +0500

    🚀 init `auth_emitter`

--- angular-auth/src/app/pages/home/home.component.ts
+++ angular-auth/src/app/pages/home/home.component.ts
@@ -1,19 +1,30 @@
-import { Emitters } from './../../emitters/emitters';
 import { CookieService } from 'ngx-cookie-service';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../../services/user.service';
+import { Emitters } from 'src/app/emitters/emitters';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  authenticated: boolean = false;
   message: any;
   jwt: string = this.cookie.get('jwt');
-  refresh_token: string = this.cookie.get('refresh');
-  constructor(private cookie: CookieService, private user: UserService) {}
+  constructor(
+    private cookie: CookieService,
+    private user: UserService,
+  ) {}
 
   ngOnInit(): void {
-      this.user.verify(this.jwt, this.refresh_token);
+    Emitters.authEmitter.subscribe((auth: boolean) => {
+      this.authenticated = auth;
+    });
+    if (this.user.verify()) {
+      Emitters.authEmitter.emit(true);
+      console.log('ok bro 😂');
+    } else {
+      Emitters.authEmitter.emit(false);
+    }
   }
 }
