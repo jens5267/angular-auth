801eee1725442e6708569c2bb4ded6b4c9561fac
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-06-09 00:22:20 +0500

    :new: Provider and Component

--- angular-auth/src/app/app.module.ts
+++ angular-auth/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './guards/auth.guard';
 import { UserService } from './services/user.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -17,6 +18,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
+import { CrudComponent } from './pages/crud/crud.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,7 @@ import { CookieService } from 'ngx-cookie-service';
     LoginComponent,
     RegisterComponent,
     ErrorPageComponent,
+    CrudComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +39,7 @@ import { CookieService } from 'ngx-cookie-service';
     FormsModule,
     HttpClientModule,
   ],
-  providers: [CookieService, UserService],
+  providers: [CookieService, UserService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
