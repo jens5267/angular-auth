fda8f6cf3b26b3c5a0b85ead9ddc1eea45a11be9
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-06-10 20:52:12 +0500

    :+1: major changes on serializer

--- angular-auth-backend/crud/serializers.py
+++ angular-auth-backend/crud/serializers.py
@@ -3,10 +3,7 @@ from rest_framework import serializers
 from .models import Person
 
 
-class PersonSerizalizer(serializers.Serializer):
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    age = serializers.IntegerField()  
+class PersonSerizalizer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ['first_name', 'last_name', 'age']
+        fields = "__all__"    
