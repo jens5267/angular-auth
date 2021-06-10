a360fa37c107f5db1555565317f46441c3c53450
Abduaziz <abduaziz.ziyodov@mail.ru>
2021-06-10 23:34:22 +0500

    :pencil: changed route name

--- angular-auth/src/app/app-routing.module.ts
+++ angular-auth/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'persons', component: CrudComponent, canActivate: [AuthGuard] },
+  { path: 'people', component: CrudComponent, canActivate: [AuthGuard] },
   { path: '**', component: ErrorPageComponent },
 ];
 
